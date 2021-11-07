62f6e708a64035d8924d27647e6c953e73dcc79a
janherzog <jan@notecho.de>
2021-11-07 23:57:09 +0100

    replaced lombok with getters on record

--- bukkit/src/main/java/de/nebelniek/content/guild/response/GuildContentResponse.java
+++ bukkit/src/main/java/de/nebelniek/content/guild/response/GuildContentResponse.java
@@ -2,6 +2,15 @@ package de.nebelniek.content.guild.response;
 
 import lombok.Getter;
 
-@Getter
 public record GuildContentResponse(GuildResponseState state, String message) {
+
+    @Override
+    public GuildResponseState state() {
+        return state;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
 }
